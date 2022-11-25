31ba298738c7480d7635159c207cbad0e33c97cb
Kyocera-KDZA <83953184+Kyocera-KDZA@users.noreply.github.com>
2022-11-25 02:36:45 +0200

    init

---
+++ app/src/main/java/com/itada/jwfy27zw2/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.itada.jwfy27zw2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/itada/jwfy27zw2/RegisterFragment.kt
@@ -0,0 +1,20 @@
+package com.itada.jwfy27zw2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
